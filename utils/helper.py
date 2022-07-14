99179d3d0c160cae8c5952cc5c7b97f7477290ad
Brandon Campbell <brandon.campbell@microsoft.com>
2022-07-14 12:26:23 -0400

    added new functions to keep main notebook cleaner

--- utils/helper.py
+++ utils/helper.py
@@ -33,3 +33,45 @@ class Print_Helper:
             print("\tCopy duration: {}".format(activity_run.output['copyDuration']))
         else:
             print("\tErrors: {}".format(activity_run.error['message']))
+
+
+
+def is_empty(a):
+    return not a
+
+def get_all_keys(d):
+    for key, value in d.items():
+        yield key
+        if isinstance(value, dict):
+            yield from get_all_keys(value)
+
+
+def look_in_string(search_string, search_value, end_value)->list:
+    '''
+    search_string is the string that you want to search on \n
+    search_value is the character or string of characters to find \n
+    end_value is the character that you want to find
+
+    returns a list
+
+    The function is used for finding a block of text using the search_value \n
+    as the starting point and the end_value as the ending point. This allows\n
+    a user to replace all of that range with another set of text
+    '''
+    i=0
+    search_pairs = []
+    while i < len(search_string):
+        start = i
+        end = len(search_string)
+        try:
+            start = search_string.index(search_value,start,end)
+            carry = search_string.index(end_value,start+1,end)
+            found_value = search_string[start:carry]
+            search_pairs.append(found_value)
+            i += start
+
+        except:
+            print('==================>>>>>>>>>i guess i threw an error<<<<<<<==================')
+            i = len(search_string)
+
+    return search_pairs
